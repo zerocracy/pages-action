bf7ed41e815729ad73bfb2226975c194e9eb74a7
Dmitry <l.e.g.o.l.a.s.s.dm@gmail.com>
2024-11-28 19:48:41 +0300

    check .js files with eslint

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+/**
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2025 Zerocracy
+ * SPDX-License-Identifier: MIT
+ */
+
+module.exports = [
+  {
+    rules: {
+      semi: "error",
+      "prefer-const": "error"
+    },
+    ignores: ["target/**/*"]
+  }
+];
