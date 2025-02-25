2c78c6f7d364ef6397185b6ba497949a4d882a8f
Yegor Bugayenko <yegor256@gmail.com>
2025-02-25 13:49:22 +0300

    #203 faster

--- judges/add-user-names/add-user-names.rb
+++ judges/add-user-names/add-user-names.rb
@@ -4,19 +4,17 @@
 # SPDX-License-Identifier: MIT
 
 require 'fbe/octo'
-require 'fbe/conclude'
 
-Fbe.conclude do
-  on '(and
+Fbe.fb.query(
+  '(and
     (or (eq where "github") (not (exists where)))
     (exists who)
     (not (exists who_name)) (not (exists who_noname)))'
-  consider do |f|
-    n = Fbe.octo.user_name_by_id(f.who)
-    f.who_name = n
-    $loog.debug("User ##{f.who} is actually @#{f.who_name}")
-  rescue Octokit::NotFound => e
-    f.who_noname = 'not found'
-    $loog.warn("THe user ##{f.who} is absent in GitHub: #{e.message}")
-  end
+).each do |f|
+  n = Fbe.octo.user_name_by_id(f.who)
+  f.who_name = n
+  $loog.debug("User ##{f.who} is actually @#{f.who_name}")
+rescue Octokit::NotFound => e
+  f.who_noname = 'not found'
+  $loog.warn("THe user ##{f.who} is absent in GitHub: #{e.message}")
 end
