c2d0541ca1165b70353ff8673b6711d77efc3c95
Sergei Mukhin <sergei_mukhin@outlook.com>
2025-12-02 00:10:37 +0300

    #469: added test that verify og tags in vital page

---
+++ entries/vitals-contains-open-graph-tags.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025 Zerocracy
+# SPDX-License-Identifier: MIT
+
+set -e -o pipefail
+
+SELF=$1
+
+BUNDLE_GEMFILE="${SELF}/Gemfile"
+export BUNDLE_GEMFILE
+bundle exec judges eval test.fb "\$fb.insert" > /dev/null
+
+env "GITHUB_WORKSPACE=$(pwd)" \
+  'GITHUB_REPOSITORY=foo/bar' \
+  'GITHUB_REPOSITORY_OWNER=foo' \
+  'INPUT_FACTBASE=test.fb' \
+  'INPUT_OUTPUT=output' \
+  'INPUT_VERBOSE=false' \
+  'INPUT_ADLESS=false' \
+  'INPUT_GITHUB-TOKEN=THETOKEN' \
+  "${SELF}/entry.sh" 2>&1 | tee log.txt
+
+grep 'meta property="og:title"' 'output/test-vitals.html'
+grep 'meta property="og:url"' 'output/test-vitals.html'
+grep 'meta property="og:image"' 'output/test-vitals.html'
+grep 'meta property="og:type"' 'output/test-vitals.html'
