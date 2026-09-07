73ef5284c1f6f45e26d470401e5ebf1e532515e5
devil <europedreadlydevil@gmail.com>
2026-09-07 18:30:10 +0000

    #797: cover both branches of the judge

--- test/judges/test_add_bylaws_html.rb
+++ test/judges/test_add_bylaws_html.rb
@@ -9,18 +9,36 @@ require_relative '../test__helper'
 class TestAddBylawsHtml < Minitest::Test
   def test_keeps_the_bylaws_when_there_is_nothing_to_build
     fb = Factbase.new
-    pmp = fb.insert
-    pmp.what = 'pmp'
-    pmp.area = 'hr'
-    pmp.hr_bonus = '(award (give 10 "for nothing"))'
+    with_formula(fb)
     load_it('add-bylaws-html', fb)
-    assert_equal(1, fb.query('(eq what "bylaws")').each.to_a.size, fb.to_json)
+    assert_equal(1, bylaws(fb), fb.to_json)
+    without_formula(fb)
+    load_it('add-bylaws-html', fb)
+    assert_equal(1, bylaws(fb), fb.to_json)
+    with_formula(fb)
+    load_it('add-bylaws-html', fb)
+    assert_equal(1, bylaws(fb), fb.to_json)
+  end
+
+  private
+
+  def bylaws(fb)
+    fb.query('(eq what "bylaws")').each.to_a.size
+  end
+
+  def with_formula(fb)
     fb.query('(and (eq what "pmp") (eq area "hr"))').delete!
-    quiet = fb.insert
-    quiet.what = 'pmp'
-    quiet.area = 'hr'
-    quiet.hr_balance = 42
-    load_it('add-bylaws-html', fb)
-    assert_equal(1, fb.query('(eq what "bylaws")').each.to_a.size, fb.to_json)
+    f = fb.insert
+    f.what = 'pmp'
+    f.area = 'hr'
+    f.hr_bonus = '(award (give 10 "for nothing"))'
+  end
+
+  def without_formula(fb)
+    fb.query('(and (eq what "pmp") (eq area "hr"))').delete!
+    f = fb.insert
+    f.what = 'pmp'
+    f.area = 'hr'
+    f.hr_balance = 42
   end
 end
