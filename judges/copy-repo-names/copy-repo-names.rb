1d8c0f07d94e55910a944ae70fc385c28cf230df
gemshrine <113778529+gemshrine@users.noreply.github.com>
2026-09-08 18:55:54 +0300

    Refresh repository details on every run

--- judges/copy-repo-names/copy-repo-names.rb
+++ judges/copy-repo-names/copy-repo-names.rb
@@ -6,8 +6,7 @@
 require 'fbe/fb'
 require 'fbe/octo'
 
-known = Fbe.fb.query('(eq what "repo-details")').each.filter_map(&:repository).to_a
-ids = Fbe.fb.query('(exists repository)').each.map(&:repository).uniq - known
+ids = Fbe.fb.query('(exists repository)').each.map(&:repository).uniq
 return if ids.empty?
 
 repos =
@@ -18,6 +17,7 @@ repos =
   end
 
 repos.each do |id, json|
+  Fbe.fb.query("(and (eq what \"repo-details\") (eq repository #{id.inspect}))").delete!
   d = Fbe.fb.insert
   d.what = 'repo-details'
   d.where = 'github'
