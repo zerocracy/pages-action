769de6b7fe84f25aaecfbb75a55f2a1525805426
devil <europedreadlydevil@gmail.com>
2026-09-14 15:20:13 +0000

    #741: pick the earned-value fact that happened last

--- test/pages/test_eva_summary.rb
+++ test/pages/test_eva_summary.rb
@@ -36,4 +36,28 @@ class TestEvaSummary < Minitest::Test
     )
     assert(xml.xpath('/p/text()').to_s.start_with?('Not enough data'))
   end
+
+  def test_picks_the_latest_fact
+    xml = xslt(
+      "<r><xsl:value-of select=\"z:latest(/fb/f[what='earned-value'])/ev\"/></r>",
+      '
+      <fb>
+        <f>
+          <_id>10</_id>
+          <what>earned-value</what>
+          <when>2024-07-04T10:00:00Z</when>
+          <ev>90</ev>
+        </f>
+        <f>
+          <_id>9</_id>
+          <what>earned-value</what>
+          <when>2024-06-01T10:00:00Z</when>
+          <ev>10</ev>
+        </f>
+      </fb>
+      ',
+      'today' => '2024-07-05T00:00:00Z'
+    )
+    assert_equal('90', xml.xpath('/r/text()').to_s, xml)
+  end
 end
